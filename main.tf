5d8ca7beefb2dda6eb14fcd4392abc0bf0a81c59
Ubuntu <ubuntu@ip-172-31-85-140.ec2.internal>
2022-06-16 20:36:17 +0000

    second update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
+# Creating VPC(Virtual Private Cloud)
 
-# creating VPC
 module "VPC" {
   source                              = "./modules/VPC"
   region                              = var.region
@@ -7,19 +7,22 @@ module "VPC" {
   enable_dns_support                  = var.enable_dns_support
   enable_dns_hostnames                = var.enable_dns_hostnames
   enable_classiclink                  = var.enable_classiclink
+  enable_classiclink_dns_support      = var.enable_classiclink_dns_support
   preferred_number_of_public_subnets  = var.preferred_number_of_public_subnets
   preferred_number_of_private_subnets = var.preferred_number_of_private_subnets
   private_subnets                     = [for i in range(1, 8, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
   public_subnets                      = [for i in range(2, 5, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
 }
 
-#Module for Application Load balancer, this will create Extenal Load balancer and internal load balancer
+
+# Module for Application Load balancer, this will create Extenal Load balancer and internal load balancer
+
 module "ALB" {
   source             = "./modules/ALB"
-  name               = "ACS-ext-alb"
+  name               = "Project18"
   vpc_id             = module.VPC.vpc_id
-  public-sg          = module.security.ALB-sg
-  private-sg         = module.security.IALB-sg
+  public-sg          = module.SG.ALB-sg
+  private-sg         = module.SG.IALB-sg
   public-sbn-1       = module.VPC.public_subnets-1
   public-sbn-2       = module.VPC.public_subnets-2
   private-sbn-1      = module.VPC.private_subnets-1
@@ -27,24 +30,25 @@ module "ALB" {
   load_balancer_type = "application"
   ip_address_type    = "ipv4"
 }
+# Module for Security Groups
 
-module "security" {
-  source = "./modules/Security"
+module "SG" {
+  source = "./modules/SG"
   vpc_id = module.VPC.vpc_id
 }
+# Module for AutoScaling Groups
 
-
-module "AutoScaling" {
-  source            = "./modules/Autoscaling"
+module "ASG" {
+  source            = "./modules/ASG"
   ami-web           = var.ami
   ami-bastion       = var.ami
   ami-nginx         = var.ami
-  desired_capacity  = 2
-  min_size          = 2
-  max_size          = 2
-  web-sg            = [module.security.web-sg]
-  bastion-sg        = [module.security.bastion-sg]
-  nginx-sg          = [module.security.nginx-sg]
+  desired_capacity  = 1
+  min_size          = 1
+  max_size          = 1
+  web-sg            = [module.SG.web-sg]
+  bastion-sg        = [module.SG.bastion-sg]
+  nginx-sg          = [module.SG.nginx-sg]
   wordpress-alb-tgt = module.ALB.wordpress-tgt
   nginx-alb-tgt     = module.ALB.nginx-tgt
   tooling-alb-tgt   = module.ALB.tooling-tgt
@@ -52,27 +56,24 @@ module "AutoScaling" {
   public_subnets    = [module.VPC.public_subnets-1, module.VPC.public_subnets-2]
   private_subnets   = [module.VPC.private_subnets-1, module.VPC.private_subnets-2]
   keypair           = var.keypair
-
 }
 
-# Module for Elastic Filesystem; this module will creat elastic file system isn the webservers availablity
-# zone and allow traffic fro the webservers
-
+# Module for Elastic File System
 module "EFS" {
   source       = "./modules/EFS"
   efs-subnet-1 = module.VPC.private_subnets-1
   efs-subnet-2 = module.VPC.private_subnets-2
-  efs-sg       = [module.security.datalayer-sg]
+  efs-sg       = [module.SG.datalayer-sg]
   account_no   = var.account_no
+
 }
 
-# RDS module; this module will create the RDS instance in the private subnet
-
+# Module for Database
 module "RDS" {
   source          = "./modules/RDS"
   db-password     = var.master-password
   db-username     = var.master-username
-  db-sg           = [module.security.datalayer-sg]
+  db-sg           = [module.SG.datalayer-sg]
   private_subnets = [module.VPC.private_subnets-3, module.VPC.private_subnets-4]
 }
 
@@ -80,9 +81,9 @@ module "RDS" {
 module "compute" {
   source          = "./modules/compute"
   ami-jenkins     = var.ami
-  ami-sonar       = var.ami
   ami-jfrog       = var.ami
+  ami-sonar       = var.ami
+  sg-compute      = [module.SG.ALB-sg]
   subnets-compute = module.VPC.public_subnets-1
-  sg-compute      = [module.security.ALB-sg]
   keypair         = var.keypair
 }
